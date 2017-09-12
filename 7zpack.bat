fa7d967cdbe1b435318ac7a81ce259e256cfbfbf
ren.yang <goyour123@gmail.com>
2017-09-13 00:12:56 +0800

    Add 7zpack script for packing a folder without types of files

---
+++ 7zpack.bat
@@ -0,0 +1,14 @@
+@REM
+@REM Use 7-Zip to pack a folder without types of files in 7zpack.ini
+@REM
+
+@REM Set the folder to pack
+@set folder="D:\Python\Python3.5\Rabbit"
+@set zpath="C:\Program Files\7-Zip"
+@set ignore=%cd%\7zpack.ini
+
+for /f "delims=\ tokens=4" %%a in (%folder%) do @set zname=%%a
+
+pushd %folder%
+start cmd.exe /k "%zpath%\7z.exe" a -xr@%ignore% %zname% && move /y %cd%\%zname%.7z %~dp0
+popd
