0a526e8fccbbcc311aa3748c088b0a3d420ef99d
ren.yang <goyour123@gmail.com>
2017-09-13 23:35:27 +0800

    Pass the folder in the same directory of 7zpack.bat to arg1

--- 7zpack.bat
+++ 7zpack.bat
@@ -3,13 +3,17 @@
 @REM
 
 @REM Set the folder to pack
-@set folder="D:\Python\Python3.5\Rabbit"
+@set folder=D:\Python\Anaconda\Jiufen
+@REM Pass the folder in the same directory of 7zpack.bat to argument 1 
+@if exist %1 @set folder=%cd%\%1\
+
 @set zpath="C:\Program Files\7-Zip"
 @set ignore=%cd%\7zpack.ini
 
 @del *.7z
 
-@for /f "delims=\ tokens=4" %%a in (%folder%) do @set zname=%%a
+@for /f "delims=\ tokens=4" %%a in ("%folder%") do @set zname=%%a
 
-@cd %folder%
-start cmd.exe /c "%zpath%\7z.exe" a -xr@%ignore% %zname% && move /y %cd%\%zname%.7z %~dp0
+@pushd %folder%
+start cmd.exe /c "%zpath%\7z.exe" a -xr@%ignore% %~dp0%zname%.7z
+@popd
