bc6b304200a77ca5f2b71346efad955a1f099252
ren.yang <goyour123@gmail.com>
2017-09-18 00:06:19 +0800

    Consume the absolute path from arg1 instead of in the batch file

--- 7zpack.bat
+++ 7zpack.bat
@@ -2,21 +2,30 @@
 @REM Use 7-Zip to pack a folder without types of files in 7zpack.ini
 @REM
 
-@REM Set the folder to pack
-@set folder=D:\Python\Anaconda\Jiufen
-@REM Pass the folder in the same directory of 7zpack.bat to argument 1 
-@if exist %1 @set folder=%cd%\%1\
-
 @set zpath="C:\Program Files\7-Zip"
+
+@REM Pass the folder in the same directory of 7zpack.bat to argument 1 
+@for /f "delims=\ tokens=1" %%a in ("%1") do @(
+    if %%a == C: set drive=C:
+    if %%a == D: set drive=D:
+    if %%a == E: set drive=E:
+    if %%a == F: set drive=F:
+    if %%a == G: set drive=G:
+)
+
+@set folder=%1
+@if not defined drive (
+    @set folder=%cd%\%1\
+)
+
 @set ignore=%cd%\7zpack.ini
 
-@del *.7z
-
 @REM Extract the last item in folder path
+@set looppath=%folder%
 :lastitemloop
-@for /f "delims=\ tokens=1*" %%a in ("%folder%") do @(
+@for /f "delims=\ tokens=1*" %%a in ("%looppath%") do @(
     @set zname=%%a
-    @set folder=%%b
+    @set looppath=%%b
     goto lastitemloop
 )
 
