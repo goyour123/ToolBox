f524a5fc2e836d35b0389b5f223802d91341e42d
ren.yang <goyour123@gmail.com>
2018-01-05 14:03:54 +0800

    Enhance get_fwposttime.bat detecting the path of tmp.txt

--- Batch/get_fwposttime.bat
+++ Batch/get_fwposttime.bat
@@ -1,8 +1,8 @@
 @echo off
-start /wait regedit.exe /e %cd%\tmp.txt "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
+start /wait regedit.exe /e %cd%tmp.txt "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
 
-for /f "tokens=2 delims=:" %%a in ('find "FwPOSTTime" tmp.txt') do set fwposttime_hex=%%a
+for /f "tokens=2 delims=:" %%a in ('find "FwPOSTTime" %cd%tmp.txt') do set fwposttime_hex=%%a
 echo %fwposttime_hex%
 
-del /q %cd%\tmp.txt
+del /q %cd%tmp.txt
 pause
