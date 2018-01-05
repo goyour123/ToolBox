a3edf8ee992d6e0e36f99294f655815fabbab981
ren.yang <goyour123@gmail.com>
2018-01-05 23:26:53 +0800

    Enhance the output result of get_fwposttime.bat of decimal value

--- Batch/get_fwposttime.bat
+++ Batch/get_fwposttime.bat
@@ -1,8 +1,11 @@
 @echo off
 start /wait regedit.exe /e %cd%tmp.txt "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
 
-for /f "tokens=2 delims=:" %%a in ('find "FwPOSTTime" %cd%tmp.txt') do set fwposttime_hex=%%a
-echo %fwposttime_hex%
+for /f "tokens=2 delims=:" %%a in ('find "FwPOSTTime" %cd%tmp.txt') do set fwposttime_hex=0x%%a
+
+set /a fwposttime_dec=%fwposttime_hex%
+
+echo FwPOSTTime %fwposttime_hex%(%fwposttime_dec%)
 
 del /q %cd%tmp.txt
 pause
