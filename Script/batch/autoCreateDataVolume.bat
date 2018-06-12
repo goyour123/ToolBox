6db7485d66e2103b6b933027ef04b3386dada5a5
ren.yang <goyour123@gmail.com>
2018-06-12 19:16:25 +0800

    Add autoCreateDataVolume.bat for auto-creating Data volume on Windows and rename the getFwPOSTTime.bat

---
+++ Script/batch/autoCreateDataVolume.bat
@@ -0,0 +1,54 @@
+@REM =========================================================================================
+@REM                                AutoCreateDataVolume.bat
+@REM =========================================================================================
+@REM This batch file would auto-create a volume named DATA with letter D from the volume with 
+@REM letter C, and the letter D volume would consume half-size space of the letter C volume.
+@REM This file is needed to be run as administrator.
+@REM =========================================================================================
+
+@REM Create diskpart script dpSrc.txt for getting volume infomation.
+echo list volume > %~dp0dpSrc.txt
+
+@REM Check whether the Data volume exist or not.
+for /f "tokens=3,4" %%c in ('diskpart /s %~dp0dpSrc.txt^|findstr /c:" D "') do (
+  if /i %%d == DATA (
+    goto END
+  )
+)
+
+@REM Get the volume number with letter C.
+for /f "tokens=2-6" %%b in ('diskpart /s %~dp0dpSrc.txt^|findstr /c:" C "') do (
+  if /i %%c == C (
+    set cVolNum=%%b
+    set cVolSize=%%f
+  )
+)
+
+@REM Use half-size of letter C space.
+set /a "dVolSize=(%cVolSize%/2)*1000"
+
+@REM Rewrite the diskpart script dpSrc.txt again for creating volume.
+echo select volume %cVolNum% > %~dp0dpSrc.txt
+echo shrink desired=%dVolSize% >> %~dp0dpSrc.txt
+echo select disk 0 >> %~dp0dpSrc.txt
+echo create partition primary size=%dVolSize% >> %~dp0dpSrc.txt
+diskpart /s %~dp0dpSrc.txt
+
+@REM Rewrite the diskpart script dpSrc.txt for getting volume infomation again.
+echo list volume > %~dp0dpSrc.txt
+
+@REM Get the index number of new volume
+for /f "tokens=2" %%b in ('diskpart /s %~dp0dpSrc.txt^|findstr /c:" RAW "') do (
+  set dVolNum=%%b
+)
+
+@REM 
+echo select volume %dVolNum% > %~dp0dpSrc.txt
+echo format quick fs=ntfs label="DATA" >> %~dp0dpSrc.txt
+echo assign letter=D >> %~dp0dpSrc.txt
+diskpart /s %~dp0dpSrc.txt
+
+:END
+@REM Delete dpSrc.txt after execution.
+del %~dp0dpSrc.txt
+exit /b 0
