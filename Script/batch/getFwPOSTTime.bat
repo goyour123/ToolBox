2dec53a29d7a92ee792f80b6a99f5fd1c800734f
ren.yang <goyour123@gmail.com>
2019-03-19 13:01:54 +0800

    Enhance getFwPOSTTime.bat

--- Script/batch/getFwPOSTTime.bat
+++ Script/batch/getFwPOSTTime.bat
@@ -2,10 +2,13 @@
 start /wait regedit.exe /e %cd%tmp.txt "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
 
 for /f "tokens=2 delims=:" %%a in ('find "FwPOSTTime" %cd%tmp.txt') do set fwposttime_hex=0x%%a
+for /f "tokens=2 delims=:" %%a in ('find "POSTTime" %cd%tmp.txt') do set posttime_hex=0x%%a
 
 set /a fwposttime_dec=%fwposttime_hex%
+set /a posttime_dec=%posttime_hex%
 
 echo FwPOSTTime %fwposttime_hex%(%fwposttime_dec%)
+echo POSTTime %posttime_hex%(%posttime_dec%)
 
-del /q %cd%tmp.txt
+@REM del /q %cd%tmp.txt
 pause
