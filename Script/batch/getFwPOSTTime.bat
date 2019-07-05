7658b00120f35741cdf241cbb278052b21a9cbca
ren.yang <goyour123@gmail.com>
2019-07-05 13:23:11 +0800

    Fix not deleting tmp.txt

--- Script/batch/getFwPOSTTime.bat
+++ Script/batch/getFwPOSTTime.bat
@@ -1,8 +1,8 @@
 @echo off
-start /wait regedit.exe /e %cd%tmp.txt "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
+start /wait regedit.exe /e %cd%\tmp.txt "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
 
-for /f "tokens=2 delims=:" %%a in ('find "FwPOSTTime" %cd%tmp.txt') do set fwposttime_hex=0x%%a
-for /f "tokens=2 delims=:" %%a in ('find "POSTTime" %cd%tmp.txt') do set posttime_hex=0x%%a
+for /f "tokens=2 delims=:" %%a in ('find "FwPOSTTime" %cd%\tmp.txt') do set fwposttime_hex=0x%%a
+for /f "tokens=2 delims=:" %%a in ('find "POSTTime" %cd%\tmp.txt') do set posttime_hex=0x%%a
 
 set /a fwposttime_dec=%fwposttime_hex%
 set /a posttime_dec=%posttime_hex%
@@ -10,5 +10,5 @@ set /a posttime_dec=%posttime_hex%
 echo FwPOSTTime %fwposttime_hex%(%fwposttime_dec%)
 echo POSTTime %posttime_hex%(%posttime_dec%)
 
-@REM del /q %cd%tmp.txt
+del /q %cd%\tmp.txt
 pause
