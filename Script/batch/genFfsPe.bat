ae3697248ff7adca28d9ee652d4e67018759ed5c
ren.yang <goyour123@gmail.com>
2020-01-17 11:40:52 +0800

    Rename the output pe file

--- Script/batch/genFfsPe.bat
+++ Script/batch/genFfsPe.bat
@@ -1,4 +1,6 @@
 
+
+@rem Check the arguments
 @if /i [%1] == [nmake] (
   @start /wait cmd.exe /c %1 %2 %3
   @if %errorlevel% neq 0 (
@@ -6,6 +8,7 @@
   )
 )
 
+@rem Get the pe file name
 @set mkfpath=%~p3
 :LOOPMKFPATH
 @for /f "delims=\ tokens=1*" %%a in ("%mkfpath%") do @(
@@ -13,13 +16,20 @@
   @set mkfpath=%%b
   @goto LOOPMKFPATH
 )
-
 @echo %pename%
 
+@rem Get system time for naming
+@for /f "delims=/ tokens=1,2,3" %%a in ("%date:~0,10%") do @(
+  set subname=%%b%%c
+)
+@for /f "delims=: tokens=1,2,3" %%a in ("%time:~0,8%") do @(
+  set subname=%subname%%%a%%b%%c
+)
+
+@rem Gernerating the ffs
 @set ffsoutpath=%cd%\BIOS
 @set efipath=%~dp3OUTPUT
-
-..\..\..\BaseTools\Bin\Win32\GenSec.exe -s EFI_SECTION_PE32 -o %ffsoutpath%\%pename%.pe %efipath%\%pename%.efi
+..\..\..\BaseTools\Bin\Win32\GenSec.exe -s EFI_SECTION_PE32 -o %ffsoutpath%\%pename%_%subname%.pe %efipath%\%pename%.efi
 @if %errorlevel% neq 0 (
   @goto ERROR
 ) else (
@@ -31,4 +41,5 @@
 @exit /b
 
 :END
+@echo %ffsoutpath%\%pename%_%subname%.pe
 @exit /b
