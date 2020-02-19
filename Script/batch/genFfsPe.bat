3873a0e48433549db326ba0bcb9e784e8a8f3b38
ren.yang <goyour123@gmail.com>
2020-02-19 13:14:48 +0800

    Extract recvy region from rom file

--- Script/batch/genFfsPe.bat
+++ Script/batch/genFfsPe.bat
@@ -26,10 +26,38 @@
 
 @rem Get system time for naming
 @for /f "tokens=1,2,3 delims=/" %%a in ("%date:~0,10%") do @(
-  set subname=%%b%%c
+  @set subname=%%b%%c
 )
 @for /f "tokens=1,2,3 delims=: " %%a in ("%time:~0,8%") do @(
-  set subname=%subname%%%a%%b%%c
+  @set subname=%subname%%%a%%b%%c
+)
+
+@rem Extract Recovery region
+@if [%arch%] equ [IA32] (
+  @rem Set Recovery region offset and size
+  @set /a recvyoffset=0x1000000+0x83A000
+  @set /a recvysize=0x120000
+
+  @rem Get the name of rom file
+  @set romdir=%cd%\BIOS
+  @pushd %romdir%
+  @for /f "delims=" %%a in ('dir /b *.fd') do @(
+  	@set biosname=%%~na
+  	@set biossize=%%~za
+  	@goto DONE
+  )
+  :DONE
+  @popd
+
+  @rem Extract Recovery region
+  @set /a recvyend=(%recvyoffset%+%recvysize%)
+  @if [%biossize%] gtr [%recvyend%] (
+    start /b /wait ..\..\..\BaseTools\Bin\Win32\split.exe -f %romdir%\%biosname%.fd -s %recvyoffset% -p %romdir% -o temp.fd -t temp2.fd
+    start /b /wait ..\..\..\BaseTools\Bin\Win32\split.exe -f %romdir%\temp2.fd -s %recvysize% -p %romdir% -o recvyorg.fd -t temp3.fd
+  ) @else (
+    start /b /wait ..\..\..\BaseTools\Bin\Win32\split.exe -f %romdir%\%biosname%.fd -s %recvyoffset% -p %romdir% -o temp.fd -t temp2.fd
+  )
+  del /q %romdir%\temp*.fd
 )
 
 @rem Gernerating the ffs
