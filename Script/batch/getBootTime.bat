1ff465c774eb67e31710a3e29e47a9304d9e863a
ren.yang <goyour123@gmail.com>
2020-03-18 12:33:01 +0800

    Enhance for different language

--- Script/batch/getBootTime.bat
+++ Script/batch/getBootTime.bat
@@ -3,16 +3,23 @@
 @set month=%date:~5,2%
 @set day=%date:~8,2%
 
+@for /f "tokens=4,5,6,7,8 delims=,/ " %%a in ('systeminfo | find /i "System Boot Time"') do @(
+  @set boottime="%%a/%%b/%%c, %%d %%e"
+  @goto OUTPUTBOOTTIME
+)
+
 @for /f "tokens=2,3,4,5,6 delims=,/ " %%a in ('systeminfo') do @(
   @if %%a equ %year% (
     @if %%b equ %month% (
       @if %%c equ %day% (
         @set boottime="%%a/%%b/%%c, %%d %%e"
+        @goto OUTPUTBOOTTIME
       )
     )
   )
 )
 
+:OUTPUTBOOTTIME
 @pushd "C:\Users\%USERNAME%\Desktop"
 @echo %boottime% >> Boot.txt
 @popd
