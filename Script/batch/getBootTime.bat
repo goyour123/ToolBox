c7dbe43e5a1a3882116d848f7e2602c6efab4103
ren.yang <goyour123@gmail.com>
2020-03-20 13:56:09 +0800

    Bug fix for getting boot time

--- Script/batch/getBootTime.bat
+++ Script/batch/getBootTime.bat
@@ -3,16 +3,15 @@
 @set month=%date:~5,2%
 @set day=%date:~8,2%
 
-@for /f "tokens=4,5,6,7,8 delims=,/ " %%a in ('systeminfo | find /i "System Boot Time"') do @(
-  @set boottime="%%a/%%b/%%c, %%d %%e"
-  @goto OUTPUTBOOTTIME
-)
-
-@for /f "tokens=2,3,4,5,6 delims=,/ " %%a in ('systeminfo') do @(
-  @if %%a equ %year% (
-    @if %%b equ %month% (
-      @if %%c equ %day% (
-        @set boottime="%%a/%%b/%%c, %%d %%e"
+@for /f "tokens=1,2,3,4,5,6,7,8 delims=,/ " %%a in ('systeminfo') do @(
+  @if "%%a %%b %%c" == "System Boot Time" (
+    @set boottime="%%d/%%e/%%f, %%g %%h"
+    @goto OUTPUTBOOTTIME
+  )
+  @if %%b equ %year% (
+    @if %%c equ %month% (
+      @if %%d equ %day% (
+        @set boottime="%%b/%%c/%%d, %%e %%f"
         @goto OUTPUTBOOTTIME
       )
     )
