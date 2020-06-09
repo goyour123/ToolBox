582a6e52479214a26ca76dc58f5a6a704144d6b3
ren.yang <goyour123@gmail.com>
2020-06-09 10:25:09 +0800

    Remove leading zeros

--- Script/batch/getBootTime.bat
+++ Script/batch/getBootTime.bat
@@ -3,6 +3,10 @@
 @set month=%date:~5,2%
 @set day=%date:~8,2%
 
+@REM Remove leading zeros
+@for /f "tokens=* delims=0" %%a in ("%month%") do @set month=%%a
+@for /f "tokens=* delims=0" %%a in ("%day%") do @set day=%%a
+
 @for /f "tokens=1,2,3,4,5,6,7,8 delims=,/ " %%a in ('systeminfo') do @(
   @if "%%a %%b %%c" == "System Boot Time" (
     @set boottime="%%d/%%e/%%f, %%g %%h"
