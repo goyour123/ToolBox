38b82e825e7e273530d38e62eaf3cf76bf3cc44a
ren.yang <goyour123@gmail.com>
2020-08-28 16:23:19 +0800

    Add dash in delims for other format of date

--- Script/batch/getBootTime.bat
+++ Script/batch/getBootTime.bat
@@ -7,8 +7,8 @@
 @for /f "tokens=* delims=0" %%a in ("%month%") do @set month=%%a
 @for /f "tokens=* delims=0" %%a in ("%day%") do @set day=%%a
 
-@for /f "tokens=1,2,3,4,5,6,7,8 delims=,/ " %%a in ('systeminfo') do @(
-  @if "%%a %%b %%c" == "System Boot Time" (
+@for /f "tokens=1,2,3,4,5,6,7,8 delims=-,/ " %%a in ('systeminfo') do @(
+  @if "%%a %%b" == "System Boot" (
     @set boottime="%%d/%%e/%%f, %%g %%h"
     @goto OUTPUTBOOTTIME
   )
