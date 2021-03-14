b416884b70d9eb83004c9e1f773312f574329091
goyour123 <goyour123@gmail.com>
2021-03-14 22:57:33 +0800

    Add build ovmf package script

--- Script/edk2/BuildEdk2.bat
+++ Script/edk2/BuildEdk2.bat
@@ -23,6 +23,9 @@ set EMU_ARCH=X64
 set EMU_TARGET=DEBUG
 @REM Gen BuildEmulator.bat
 echo build -p EmulatorPkg\EmulatorPkg.dsc -t %EMU_TOOL_CHAIN% -a %EMU_ARCH% -b %EMU_TARGET% > %cd%\BuildEmulator.bat
+@REM Gen BuildOvmf.bat
+echo build -p OvmfPkg\OvmfPkgX64.dsc -t %EMU_TOOL_CHAIN% -a %EMU_ARCH% -b %EMU_TARGET% > %cd%\BuildOvmf.bat
+
 @REM Gen RunEmulator.bat
 echo pushd %cd%\Build\Emulator%EMU_ARCH%\%EMU_TARGET%_%EMU_TOOL_CHAIN%\%EMU_ARCH% > %cd%\RunEmulator.bat
 echo %cd%\Build\Emulator%EMU_ARCH%\%EMU_TARGET%_%EMU_TOOL_CHAIN%\%EMU_ARCH%\WinHost.exe >> %cd%\RunEmulator.bat
